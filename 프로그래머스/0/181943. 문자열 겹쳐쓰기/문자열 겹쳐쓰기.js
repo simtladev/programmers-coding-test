b202185f30f935dbe346162a18edab1a3b51c7a0
simtladev <141210256+simtladev@users.noreply.github.com>
2024-08-03 21:21:19 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,4 @@
+function solution(my_string, overwrite_string, s) {
+    const answer = my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length, my_string.length);
+    return answer;
+}
