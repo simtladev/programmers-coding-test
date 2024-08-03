84942d82f77d3e81beb78d4cbe2a6cec8e0e651a
simtladev <141210256+simtladev@users.noreply.github.com>
2024-08-03 21:23:35 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,22 @@
+function solution(ineq, eq, n, m) {
+    let answer = 0;
+    
+    
+    if(ineq === ">"){
+        if(eq === "="){
+            answer = n >= m ? 1 : 0;
+        }else{
+            answer = n > m ? 1 : 0;
+        }
+    }else {
+        if(eq === "="){
+            answer = n <= m ? 1 : 0;
+        }else{
+            answer = n < m ? 1 : 0;
+        }
+    }
+    
+    
+    
+    return answer;
+}
