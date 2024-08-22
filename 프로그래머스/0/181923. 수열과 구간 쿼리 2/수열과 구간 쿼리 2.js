d13a4a96f5dc4c817876d344681fff3f1146b0e6
simtladev <141210256+simtladev@users.noreply.github.com>
2024-08-22 13:19:29 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 17.64 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -2,11 +2,7 @@ function solution(arr, queries) {
     const answer = [];
     
     queries.forEach(([s,e,k])=>{
-        const a = arr.slice(s, e + 1).filter((item)=> item > k).sort((a, b) => a - b);
-        if(!a.length){
-         return answer.push(-1);  
-        };
-        return answer.push(a[0]);
+        return answer.push(arr.slice(s, e + 1).filter((item)=> item > k).sort((a, b) => a - b)[0] || -1);
     });
     
     
