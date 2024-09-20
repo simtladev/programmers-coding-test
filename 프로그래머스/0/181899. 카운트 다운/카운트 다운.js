abb537627f96aeadc2cb62270c216af84b44cfa6
simtladev <141210256+simtladev@users.noreply.github.com>
2024-09-20 09:58:27 +0900

    [level 0] Title: 카운트 다운, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -1,9 +1,13 @@
 function solution(start_num, end_num) {
-    const result = [];
+    // const result = [];
     
-    for(let i = start_num; i >= end_num; i--){
-        result.push(i);
-    }
+    // for(let i = start_num; i >= end_num; i--){
+    //     result.push(i);
+    // }
+    
+    const result = Array(start_num - end_num  + 1).fill(start_num).map((v, i)=> v - i);
+    
+    
     
     
     return result;
