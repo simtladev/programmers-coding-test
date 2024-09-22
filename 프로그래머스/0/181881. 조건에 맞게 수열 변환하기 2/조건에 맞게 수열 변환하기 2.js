319574facc04d914951f4bc6e3b541a9db073877
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-22 16:47:32 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 376.64 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -6,7 +6,7 @@ function solution(arr) {
     while(JSON.stringify(arr1) !== JSON.stringify(arr2)){
         arr2 = [...arr1];
         
-        const v = arr1.map((value)=> {
+        const update_arr = arr1.map((value)=> {
             if(value >= 50 && value % 2 === 0){
                 return value / 2;
             }
@@ -16,7 +16,7 @@ function solution(arr) {
             return value;
         });
         
-        arr1 = [...v];
+        arr1 = [...update_arr];
         count++;
     }
     
