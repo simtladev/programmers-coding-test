27f0975b6f7b5f7274b0ecb19c0382e730c2dd56
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-23 10:37:46 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,12 +1,16 @@
 function solution(myString) {
-    const answer = [...myString].map((v)=> {
-        if(v === "a"){
-           return v.toUpperCase(); 
-        }
-        if(v !== "A"){
-            return v.toLowerCase();
-        }
-         return v;
-    }).join("");
+    // const answer = [...myString].map((v)=> {
+    //     if(v === "a"){
+    //        return v.toUpperCase(); 
+    //     }
+    //     if(v !== "A"){
+    //         return v.toLowerCase();
+    //     }
+    //      return v;
+    // }).join("");
+    
+    const answer = myString.toLowerCase().replaceAll('a','A');
+    
+    
     return answer;
 }
