44f2c0956eef37574759136f608142b3030b24d8
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-24 11:42:41 +0900

    [level 0] Title: 그림 확대, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,9 +1,8 @@
 function solution(picture, k) {
     const answer = picture.reduce((acc, cur)=>{
         const line = [...cur].map((dote)=> dote.repeat(k)).join("");
-        for(let i = 0; i < k; i++){
-            acc.push(line);    
-        }
+        acc.push(...Array(k).fill(line));    
+        
         return acc;
     },[])
     
