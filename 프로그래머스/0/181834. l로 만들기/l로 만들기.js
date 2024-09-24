8f7c9ab2037d0134274c0b632ce3d1516b3874a0
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-24 10:27:13 +0900

    [level 0] Title: l로 만들기, Time: 11.81 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,5 +1,8 @@
 function solution(myString) {
-    const answer = [...myString].map((v)=> v.charCodeAt() < "l".charCodeAt() ?  'l' : v).join("");
+    // const answer = [...myString].map((v)=> v.charCodeAt() < "l".charCodeAt() ?  'l' : v).join("");
+    
+    
+    const answer = [...myString].map((v)=> v < "l" ?  "l" : v).join("");
     
     
     // l = 108
