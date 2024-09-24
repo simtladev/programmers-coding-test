15dc97af9b90ed11426e444c7dd358bc110a12a2
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-24 11:56:46 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.17 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,7 +1,7 @@
 function solution(n) {
-    const answer = Array(n).fill([]).map((_, i)=> {
+    const answer = Array.from({ length: n }, (_, i) => {
         const a = Array(n).fill(0);
-        a[i] = 1;
+        a[i] = 1;  // i번째 값만 1로 설정
         return a;
     });
     return answer;
