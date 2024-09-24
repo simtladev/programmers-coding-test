a80fe59c4edc2705468daa3292a661448badb487
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-24 10:41:55 +0900

    [level 0] Title: 정수 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,4 +1,6 @@
 function solution(num_list, n) {
-    const answer = num_list.find((v)=> v ===n) ? 1 : 0;
+    // const answer = num_list.find((v)=> v ===n) ? 1 : 0;
+    
+    const answer = +num_list.includes(n);
     return answer;
 }
