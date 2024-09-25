1d1c25607f9c47640c6e1c275953f69aa42b0a89
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-25 11:59:18 +0900

    [level 0] Title: 문자열 밀기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,1 +1,8 @@
-let solution=(a,b)=>(b+b).indexOf(a)
+function solution(A, B) {
+    if (A.length !== B.length) return -1;
+    const combined = B + B;
+    const index = combined.indexOf(A);
+    return index !== -1 ? index : -1;
+}
+
+// let solution=(a,b)=>(b+b).indexOf(a)
