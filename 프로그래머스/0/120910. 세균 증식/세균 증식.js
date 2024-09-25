8be4f68d65bed4e74906e583a05be9c6410d34e4
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-25 11:05:19 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,9 +1,19 @@
 function solution(n, t) {
-    let answer = n;
-    for(let i = 0; i< t; i++){
-       answer += answer; 
-    }
+    // let answer = n;
+    // for(let i = 0; i< t; i++){
+    //    answer += answer; 
+    // }
+    // return answer;
+    const answer =  n * Math.pow(2, t)
     return answer;
+    
+    // 1 2 4 8 
+    // 3 6 12     3 배 
+    // 4 8 16 32  4 배 
+    //n은 배율 t는 횟수
+    
+    
+    
 }
 
 
