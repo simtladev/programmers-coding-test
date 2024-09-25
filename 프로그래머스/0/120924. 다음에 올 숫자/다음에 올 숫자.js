9cadc7c629e7c88ae313ee6bcb37ca3e93b9ce93
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-25 17:29:52 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,14 @@
+function solution(common) {
+    if ((common[1]-common[0])==(common[2]-common[1])){
+        return common.pop() + common[1] - common[0];
+    }
+    else{
+        return common.pop()*common[1]/common[0];
+    }
+}
+
+
+
+
+// 2 4 8 16
+// 3 6 12 24
