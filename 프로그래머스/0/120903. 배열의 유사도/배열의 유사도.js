5c5594916f4334bad05668ace15d48d56c6ef759
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-25 10:44:48 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,9 @@
 function solution(s1, s2) {
-    const answer  = s1.reduce((acc, cur)=>{
-        return s2.includes(cur) ? ++acc : acc;
-    },0)
+    // const answer  = s1.reduce((acc, cur)=>{
+    //     return s2.includes(cur) ? ++acc : acc;
+    // },0);
+    
+    
+    const answer = s1.filter((v)=> s2.includes(v)).length;
     return answer;
 }
