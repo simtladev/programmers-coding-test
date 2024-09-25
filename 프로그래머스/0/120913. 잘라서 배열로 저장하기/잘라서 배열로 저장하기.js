bdad7c0a32984c99ccb13c2664dceb703342184b
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-25 11:34:15 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,7 +1,13 @@
 function solution(my_str, n) {
-    const answer = [];
-    for(let i =0; i < my_str.length; i+= n){
-        answer.push(my_str.slice(i, n + i));
-    }
+    // const answer = [];
+    // for(let i =0; i < my_str.length; i+= n){
+    //     answer.push(my_str.slice(i, n + i));
+    // }
+    // return answer;
+    
+    const answer = Array.from({ length: Math.ceil(my_str.length / n) }, (_, i) =>{
+        return my_str.slice(i * n, i * n + n)
+    });
+    
     return answer;
 }
