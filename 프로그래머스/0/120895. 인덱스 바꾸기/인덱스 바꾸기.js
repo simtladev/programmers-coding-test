2994b59db8a86fcc1dd46540026a939d2b3f26da
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-26 16:00:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,7 @@
 function solution(my_string, num1, num2) {
     const answer = [...my_string];
-    const a = my_string.slice(num1, num1 +1);
-    const b = my_string.slice(num2, num2 +1);
-    answer[num1] = b;
-    answer[num2] = a;
+    
+    [answer[num1], answer[num2]] = [answer[num2], answer[num1]];
 
 
     return answer.join("");
