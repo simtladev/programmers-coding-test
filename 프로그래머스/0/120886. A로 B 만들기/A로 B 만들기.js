64a705943f846a36c645d4a02a2534e0e60170bd
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-27 14:57:06 +0900

    [level 0] Title: A로 B 만들기, Time: 0.66 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,14 +1,19 @@
 function solution(before, after) {
-    const afterArr = [...after]
-    const answer = [...before].every((v)=>{
-        const currentIndex = afterArr.indexOf(v);
-        if(currentIndex !== -1){
-           afterArr.splice(currentIndex, 1);
-           return true; 
-        }
-        return false;
-    });
+//     const afterArr = [...after]
+//     const answer = [...before].every((v)=>{
+//         const currentIndex = afterArr.indexOf(v);
+//         if(currentIndex !== -1){
+//            afterArr.splice(currentIndex, 1);
+//            return true; 
+//         }
+//         return false;
+//     });
 
 
-    return answer ? 1 : 0; 
+//     return answer ? 1 : 0; 
+    
+    const answer = [...before].sort().join("") === [...after].sort().join("") ? 1 : 0;
+    
+    return answer;
+    
 }
