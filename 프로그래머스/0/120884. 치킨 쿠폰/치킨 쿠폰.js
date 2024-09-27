81d6ddf511c4fea36afa5a0b6394637aa37e6310
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-27 16:51:07 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,16 +1,16 @@
 function solution(chicken) {
-    let answer = 0;
-    let count = 0;
-    while(chicken >= 0){
-        if(count === 10){
-            chicken++;
-            answer++;
-            count = 0;
-        }
-        count++;
-        chicken--;
-    }
-    
+//     let answer = 0;
+//     let count = 0;
+//     while(chicken >= 0){
+//         if(count === 10){
+//             chicken++;
+//             answer++;
+//             count = 0;
+//         }
+//         count++;
+//         chicken--;
+//     }
+    const answer = parseInt((chicken - 1) / 9) 
     
     return answer;
 }
