bac1ebc7818530a39c5a98975a47574221830e4e
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-28 21:46:19 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,10 +1,20 @@
 function solution(numbers, direction) {
-    if(direction === "left"){
-        return [...numbers.slice(1, numbers.length), ...numbers.slice(0, 1)];
-    }else{
+//     if(direction === "left"){
+//         return [...numbers.slice(1, numbers.length), ...numbers.slice(0, 1)];
+//     }else{
         
-        return [...numbers.slice(-1), ...numbers.slice(0, numbers.length -1)];
-    }
-    
+//         return [...numbers.slice(-1), ...numbers.slice(0, numbers.length -1)];
+//     }
+    let answer = []
+
+     if ("right" == direction) {
+            numbers.unshift(numbers.pop());
+     } else {
+            numbers.push(numbers.shift());
+     }
+
+    answer = numbers;
+
+    return answer;
 }
 
