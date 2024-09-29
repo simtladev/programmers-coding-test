f73860a2172630fd78feaf44f17f4a5d57ba251f
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-29 17:31:54 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,4 +1,6 @@
 function solution(my_string, letter) {
-    const answer = my_string.split(letter).join("");
+    // const answer = my_string.split(letter).join("");
+    
+    const answer = my_string.replaceAll(letter, "");
     return answer;
 }
