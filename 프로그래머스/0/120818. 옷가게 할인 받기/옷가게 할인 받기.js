9c94e05def8c4b2512a2204d21853e2b8ac86a56
김진우 <141210256+simtladev@users.noreply.github.com>
2024-09-29 17:43:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,19 @@
+function solution(price) {
+    
+    if(price >= 500000){
+        return Math.floor(price - (price * (20/100)));
+    }
+    
+    if(price >= 300000){
+        return Math.floor(price - (price * (10/100)));
+    }
+    
+    if(price >= 100000){
+        return Math.floor(price - (price * (5/100)));
+    }
+    
+ 
+   
+    
+    return price;
+}
