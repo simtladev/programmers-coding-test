18383f258e56ac7c8ef2cbf9273b6dc9821b44cd
김진우 <141210256+simtladev@users.noreply.github.com>
2024-10-02 23:25:16 +0900

    [level 0] Title: 다항식 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -3,8 +3,10 @@ function solution(polynomial) {
         return cur.includes("x") ? [x + Math.max(+cur.split("x")[0], 1), c] : [x, c + +cur]
     },[0, 0]);
     
-    const answer = (x ? `${x === 1 ? '' : x}x` : '') + (c ? `${x ? " + " : ''}${c}` : '')
+    const xPart = x ? `${x === 1 ? '' : x}x` : ''; 
+    const cPart = c ? `${x ? " + " : ''}${c}` : ''; 
     
+    const answer = xPart + cPart
     
     return answer;
 }
