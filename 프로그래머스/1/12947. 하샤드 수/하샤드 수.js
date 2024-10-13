1e016fc1da960a46bc84def5672fed0a3b08eec3
김진우 <141210256+simtladev@users.noreply.github.com>
2024-10-14 00:36:02 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,4 @@
 function solution(x) {    
-    const a = [...x.toString()].reduce((acc, cur)=> acc + +cur,0) ;
-    const answer = x % a === 0 ? true :false
-    
+    const answer = x % [...x.toString()].reduce((acc, cur)=> acc + +cur,0) === 0 ? true : false;
     return answer;
 }
