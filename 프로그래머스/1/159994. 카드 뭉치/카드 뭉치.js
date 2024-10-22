1ee97c8f6060a7158f28ffc111a04c14e993e254
김진우 <141210256+simtladev@users.noreply.github.com>
2024-10-22 11:45:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -2,18 +2,6 @@ function solution(cards1, cards2, goal) {
     let i = 0;
     let j = 0;
     
-    
-    var answer = goal.every((v)=> {
-        if(v === cards1[i]){
-            i++;
-            return true;
-        }
-        
-        if(v === cards2[j]){
-            j++;
-            return true;
-        }
-        return false;
-    });
+    const answer = goal.every((v)=> v === cards1[i] ? ++i : v === cards2[j] ? ++j : false);
     return answer ? "Yes" : "No";
 }
