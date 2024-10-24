cb3a30499d2d282f3b2b8830720638a1f5c000a9
김진우 <simtladev@gmail.com>
2024-10-24 12:46:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 2086.09 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,22 @@
+function solution(number, limit, power) {
+    const numberList = Array.from({length: number}, (_,i)=> i + 1);
+    const answer = numberList.reduce((acc, cur)=>{
+        const currentPower = Array.from({length: Math.sqrt(cur)}, (_, i)=> i + 1).reduce((acc2, cur2)=>{
+            if(cur % cur2 === 0){
+                if(cur / cur2 === cur2){
+                    return acc2 += 1
+                }else{
+                    return acc2 += 2;
+                }
+            }
+            return acc2;
+        }, 0);
+    
+        
+        return currentPower <= limit ? acc += currentPower : acc += power;
+    }, 0)
+        
+    return answer; 
+}
+
+124
