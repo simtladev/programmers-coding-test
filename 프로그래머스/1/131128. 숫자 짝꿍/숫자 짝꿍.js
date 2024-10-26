4fc4b6f8f5020ffc9f4eb1ca15c2889a7607794e
김진우 <simtladev@gmail.com>
2024-10-26 13:12:29 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2085.75 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,19 +1,5 @@
 function solution(X, Y) {
-//     const YList = [...Y];
-//     const temp = [...X].reduce((acc, cur)=> {
-//         if(YList.indexOf(cur) !== -1){
-//             acc.push(+cur)
-//             YList.splice(YList.indexOf(cur), 1);
-//         }
-        
-//         return acc;
-//     }, []).sort((a,b)=> b - a).join("");
     
-    
-//     const answer = temp === "" ? "-1" : (+temp).toString()
-    
-//     return answer;
-        
     let answer = "";
     for(let i = 9; i >= 0; i--){
         const xLength = [...X].filter((v)=> +v === i).length;
@@ -23,7 +9,6 @@ function solution(X, Y) {
     }
     
     if(answer === "") return "-1";
-    if(+answer === 0) return "0"
-    
-    return [...answer].sort((a,b) => +b - +a).join("");
+    if(+answer === 0) return "0";
+    return answer;
 }
