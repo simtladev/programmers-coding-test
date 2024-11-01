e3bf3f59380ace5032ee628c12fcda733c9bc082
김진우 <simtladev@gmail.com>
2024-11-01 15:25:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -3,13 +3,9 @@ function solution(new_id) {
                 .replace(/[^a-z0-9._-]/g, '')
                 .replace(/\.{2,}/g, '.')
                 .replace(/^\.|\.$/g, '')
-                .replace(/^$/, 'a');
+                .replace(/^$/, 'a')
+                .slice(0, 15).replace(/\.$/, '')
     
-    if (answer.length >= 16) {
-        answer = answer.slice(0, 15).replace(/\.$/, '');
-    }else if(answer.length <=2 ){
-        answer = answer.padEnd(3, answer[answer.length - 1]);
-    }
     
-    return answer;
+    return answer.length <=2 ? answer.padEnd(3, answer[answer.length - 1]) : answer;
 }
