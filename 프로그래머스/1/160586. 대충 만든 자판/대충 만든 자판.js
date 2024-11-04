d2c5c327b16b5ab67d4cb9502619d01a08cd5f2a
김진우 <simtladev@gmail.com>
2024-11-05 08:16:18 +0900

    [level 1] Title: 대충 만든 자판, Time: 47.08 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,13 @@
+function solution(keymap, targets) {
+    const answer = targets.reduce((result, target)=>{
+        const count = [...target].reduce((count, keyword)=>{
+            if(count === -1) return count;
+            const clicks = keymap.map((v)=> v.indexOf(keyword)).filter((v) => v !== -1);
+            if(!clicks.length) return -1;
+            return count + Math.min(...clicks) + 1;
+        }, 0)
+        result.push(count);
+        return result;
+    },[]);
+    return answer;
+}
