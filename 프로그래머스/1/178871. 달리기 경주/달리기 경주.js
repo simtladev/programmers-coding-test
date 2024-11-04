746591c6fd66a69646a0a7de60e106f5f26a7082
김진우 <simtladev@gmail.com>
2024-11-05 08:58:26 +0900

    [level 1] Title: 달리기 경주, Time: 161.70 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -5,9 +5,7 @@ function solution(players, callings) {
         const temp = players[i-1];
     
         [players[i-1], players[i]] = [players[i], players[i - 1]];
-        playerMap[calling] = i - 1;
-        playerMap[temp] = i;
-        
+        [playerMap[calling], playerMap[temp]] = [i - 1, i];
     }
 
     return players;
