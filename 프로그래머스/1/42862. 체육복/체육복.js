9e2f3b74e712dfa553a61bd3bcea9af0cb02e756
김진우 <simtladev@gmail.com>
2024-11-05 10:45:40 +0900

    [level 1] Title: 체육복, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,20 @@
+function solution(n, lost, reserve) {
+    let answer = n - lost.length;
+    
+    let realLost= lost.filter((v)=>!reserve.includes(v));
+    let realReserve= reserve.filter((v)=>!lost.includes(v));
+    answer+= lost.length - realLost.length;
+    
+    realLost.sort((a,b)=>a-b);
+    realReserve.sort((a,b)=>a-b);
+    
+     for(const lost of realLost){
+        const temp = realReserve.findIndex((v)=> v === lost - 1 || v === lost + 1);
+        if(temp !== -1){
+            realReserve.splice(temp, 1);
+            answer++;
+        }
+     }
+    
+    return answer;
+}
