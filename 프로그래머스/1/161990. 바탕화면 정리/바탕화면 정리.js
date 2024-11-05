37e72de96f1096f0cc18b8708a63e82f743a8e75
김진우 <simtladev@gmail.com>
2024-11-05 11:48:07 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,19 +1,16 @@
 function solution(wallpaper) {
-  const X = [];
-  const Y = [];
-
+  let minX = Infinity, maxX = -Infinity, minY = Infinity, maxY = -Infinity;
 
   for (let i = 0; i < wallpaper.length; i++) {
     for (let j = 0; j < wallpaper[i].length; j++) {
       if (wallpaper[i][j] === "#") {
-        Y.push(i);
-        X.push(j);
+        minX = Math.min(minX, j);
+        maxX = Math.max(maxX, j);
+        minY = Math.min(minY, i);
+        maxY = Math.max(maxY, i);
       }
     }
   }
 
-  X.sort((a, b) => a - b);
-  Y.sort((a, b) => a - b);
-
-  return [Y[0], X[0], Y[Y.length - 1] + 1, X[X.length - 1] + 1];
+  return [minY, minX, maxY + 1, maxX + 1];
 }
