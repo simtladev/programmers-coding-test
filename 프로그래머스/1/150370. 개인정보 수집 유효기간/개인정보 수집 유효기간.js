94dfacfc85e7d42f4d8981873cf0317bf3193c5f
김진우 <simtladev@gmail.com>
2024-11-16 09:30:42 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.61 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -2,19 +2,18 @@ function solution(today, terms, privacies,i) {
     const termsObject = Object.fromEntries(terms.map((v)=> v.split(" ")));
     
     const answer = privacies.reduce((result, privacy, i)=>{
-        const [privacyDate, term] = privacy.split(" ");
-        const termDate = +termsObject[term]
-        let [year, month, day] = privacyDate.split(".").map((v)=> +v);
+        const [privacyStr, term] = privacy.split(" ");
+        const termDate = +termsObject[term];
+        let [year, month, day] = privacyStr.split(".").map((v)=> +v);
         month += termDate;
         if(month > 12){
             year += Math.floor(month / 12);
             month = month % 12;
         }
         const [tYear, tMonth, tDay] = today.split(".");
-        
-        const a = new Date(year, month, day);
-        const b = new Date(tYear, tMonth, tDay);
-        if(a.getTime() <= b.getTime()){
+        const privacyDate = new Date(year, month, day);
+        const todayDate = new Date(tYear, tMonth, tDay);
+        if(privacyDate.getTime() <= todayDate.getTime()){
             result.push(i + 1);
         }
         
