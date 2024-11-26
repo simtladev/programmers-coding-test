ad8e680419d9837a88b2247084d1b96a8330a798
김진우 <simtladev@gmail.com>
2024-11-26 19:46:16 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,4 +1,6 @@
 function solution(my_string, n) {
     const answer = [...my_string].map((v)=> v.repeat(n)).join("");
+    
+    // const answer = [...my_string].map((v)=> Array(n).fill(v)).flat().join("");
     return answer;
 }
