71f96aeffa96060d5dd5f847d032ad56215518cc
김진우 <simtladev@gmail.com>
2024-11-26 19:24:50 +0900

    [level 0] Title: 양꼬치, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,4 +1,4 @@
 function solution(n, k) {
-    const answer = n * 12000 + k * 2000 - Math.floor(n / 10) * 2000;
+    const answer = n * 12000 + k * 2000 - Math.trunc(n / 10) * 2000;
     return answer;
 }
