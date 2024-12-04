2dbb964fd70f1ecd3b11086ec47d913c24795b4f
김진우 <simtladev@gmail.com>
2024-12-04 21:12:49 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -3,8 +3,6 @@ function solution(num_list) {
     const a = num_list.reduce((a , b)=> a * b,1);
     const b = num_list.reduce((a , b)=> a + b,0) ** 2;
     
-    console.log(a,b)
-    
-    var answer = a < b ? 1 : 0;
+    const answer = a < b ? 1 : 0;
     return answer;
 }
