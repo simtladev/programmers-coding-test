a5d786f25a874bbafd2e11d251279ffd84d1e6ea
김진우 <simtladev@gmail.com>
2024-12-06 22:07:12 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,11 +1,3 @@
 function solution(start_num, end_num) {
-    const answer = [];
-    
-    
-    for(let i = start_num; i <= end_num; i++){
-        answer.push(i);
-    }
-    
-    
-    return answer;
+    return Array(end_num-start_num+1).fill(start_num).map((v, i) => v + i);
 }
